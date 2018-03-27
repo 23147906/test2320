3d6f4e31a8b9a916dcfb6e6e27eccd06635c3c66
23147906 <32630947+23147906@users.noreply.github.com>
2018-03-27 23:43:46 +0800

    Add files via upload

---
+++ while循环.py
@@ -0,0 +1,20 @@
+#! -*- coding:utf-8 -*-
+# Author:Win_Li   23147
+# Date:2018-03-19 9:42
+
+cont = 0
+# while cont <= 100:
+#     print(cont)
+#     cont += 1
+
+# while cont <= 100:
+#     if cont % 2 == 0:
+#         print(cont)
+#
+#     cont += 1
+
+while cont <= 100:
+    if cont % 2 == 1:
+        print(cont)
+    cont += 1
+
