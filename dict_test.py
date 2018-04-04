409c9e4d2e44f2ecf74a98cfc43ebae33072db6f
23147906 <32630947+23147906@users.noreply.github.com>
2018-04-04 22:57:05 +0800

    Add files via upload

---
+++ dict_test.py
@@ -0,0 +1,42 @@
+#! -*- coding:utf-8 -*-
+# Author:Win_Li   23147
+# Date:2018-03-23 23:13
+
+
+# d = {}
+# d[1] = 'a'
+# d[2] = 'A'
+# d[3] = 'b'
+# d[4] = 'c'
+# d[5] = 'h'
+#
+# print(d)
+# # for i in d:
+# #     print(i, d[i])
+# #     print(i)
+# print(d.pop(2))
+# print(d)
+
+# l2 = [1, 2, 3, 5]
+#
+# l2[1:3] = 'a', 'd', 'c', 'g'
+# print(l2)
+#
+# for a, b in enumerate(l2):
+#     print(a, b)
+# s = 'win'
+# s2 = ('w', 'n')
+# # print(s.center(1, '!'))
+# # print(s.count('i'))
+# # print(s.endswith("i"))
+# # print(s.find('w'))
+# # print(s.format('de', '333'))
+# # print(s.isdigit())
+# # print('-'.join((s2)))
+# print(hash(s))
+# print(hash(s2))
+
+d = {1: 'a', 2: 'b'}
+
+print(d.pop(2))
+
