e381c183e144c98a54a142e3eb932af10126bf12
23147906 <32630947+23147906@users.noreply.github.com>
2018-04-11 23:53:42 +0800

    Add files via upload

---
+++ test005.py
@@ -0,0 +1,26 @@
+#! -*- coding:UTF-8 -*-
+# Author:Win_Li   23147
+# Date:2018-03-26 23:01
+
+
+s = 'alex'
+l = list(s)
+print(l)
+
+t = tuple(s)
+print(t)
+
+li = ["alex", "seven"]
+t1 = tuple(li)
+print(t1)
+
+tu = ('Alex', "seven")
+l1 = list(tu)
+print(l1)
+
+
+li = ["alex", "seven"]
+d = {}
+# d = d.fromkeys(li, range(10))
+# print(d)
+
