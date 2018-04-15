03f8532103d303eed23048bcb81565ed2a7838c3
23147906 <32630947+23147906@users.noreply.github.com>
2018-04-15 22:04:29 +0800

    Add files via upload

--- test002.py
+++ test002.py
@@ -1,35 +1,37 @@
 #! -*- coding:UTF-8 -*-
 # Author:Win_Li   23147
-# Date:2018-03-26 22:26
+# Date:2018-03-28 0:27
 
 
-li = ['alex', 'eric', 'rain']
-print(len(li))
-li.append('seven')
-print(li)
-li.insert(0, 'Tony')
-print(li)
-li.insert(1, 'Kelly')
-print(li)
-li.remove('eric')
-print(li)
-print(li.pop(1), li)
+# L1 = [5, 3, 2, 7, 6, 23, 41, 24, 33, 4, 1]
+#
+# for i in range(len(L1)-1):
+#     for a in range(len(L1)-i-1, 0, -1):
+#         if L1[a-1] > L1[a]:
+#             L1[a-1], L1[a] = L1[a], L1[a-1]
+#
+#     print(L1)
 
-del li[2]
-print(li)
-del li[1:4]
-print(li)
-li = ['alex', 'eric', 'rain']
-li.reverse()
-print(li)
-li.sort()
-print(li)
-for i in range(len(li)):
-    print(i)
+# list1 = [1, 3, 2, 7, 6, 23, 41, 24, 33, 85, 5]
+#
+# # for i in range(len(list1)-1):  # 因为是两个元素相比较，所以n(元素个数)-1次就够了
+# #     for index in range(len(list1)-i-1):  # 同上
+# #         if list1[index] < list1[index + 1]:
+# #             list1[index], list1[index + 1] = list1[index+1], list1[index]
+# #             print(list1)
+#
+# add = 0
+# for i in range(2, 100):
+#     for a in range(2, i):
+#         if i % a == 0:
+#             break
+#     else:
+#         print(i)
+#         add += i
+# print(add)
 
-for a, b in enumerate(li, 100):
-    print(a, b)
 
-for i in li:
-    print(i)
+print('\033[31;1m%s\033[0m' % '输出红色字符')
+
+
 
