a14a1571775fa03118f5d219c4808aed804577b1
23147906 <32630947+23147906@users.noreply.github.com>
2018-04-15 22:04:03 +0800

    Add files via upload

--- test001.py
+++ test001.py
@@ -1,42 +1,70 @@
 #! -*- coding:UTF-8 -*-
 # Author:Win_Li   23147
-# Date:2018-03-26 22:06
+# Date:2018-03-27 21:18
 
 
-li = ['alex', 'eric', 'rain']
-print(len(li))
+# l1 = [11, 22, 33]
+# l2 = [22, 33, 44]
+#
+# s1 = set(l1)
+# s2 = set(l2)
+#
+# s3 = s1.difference(s2)
+# s4 = s1 - s2
+# print(s3, s4)
+#
+# s3 = s2.difference(s1)
+# s4 = s2 - s1
+# print(s3, s4)
+#
+# s3 = s1.symmetric_difference(s2)
+# s4 = s1 ^ s2
+# print(s3, s4)
+#
+# s3 = s1.intersection(s2)
+# s4 = s1 & s2
+# print(s3, s4)
+#
+# s3 = s1.union(s2)
+# s4 = s1 | s2
+#
+# print(s3, s4)
+#
+#
+# for i in range(0, 101):
+#     print(i)
+
+# a = 100
+# while a > 0:
+#     print(a)
+#     a -= 1
+#
+# a = 1
+# while a <= 100:
+#     print(a)
+#     a += 1
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+for i in range(1, 10):
+    for a in range(1, i+1):
+        s = i * a
+        print('%s*%s=%s  ' % (i, a, s), end='')
+    print('')
 
 
-li.append('seven')
-print(li)
+# 1*1 = 1
+# 2*1 = 2 2*2 = 4
+# 3*1 = 3 3*2 = 6 3*3 =9
 
-li.insert(0, 'Tony')
-print(li)
 
-li[1] = 'Kelly'
-print(li)
-
-li.remove('eric')
-print(li)
-print(li.pop(1))
-print(li)
-
-del li[2]
-print(li)
-
-del li[1:4]
-print(li)
-
-li = ['alex', 'eric', 'rain']
-
-li.reverse()
-print(li)
-
-for i in range(len(li)):
-    print(i)
-
-for index, i in enumerate(li, 100):
-    print(index, i)
-
-for i in li:
-    print(i)
+add = 0
+for i in range(2, 100):
+    for a in range(2, i):
+        if (i % a) == 0:
+            break
+    else:
+        print(i)
+        add += i
+print(add)
